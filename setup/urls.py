a2facb3b5cd537504d89efdaf7cd30393d4a6640
ahmet.azanpa <ahmetselcuk.azanpa@aurorabilisim.com>
2024-07-11 22:16:39 +0300

    converstation added.

--- setup/urls.py
+++ setup/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dahboard/', include('dashboard.urls')),
+    path("inbox/", include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
